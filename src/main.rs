772294641f1297275500dbff15c6a3fa85467c97
RdotSilva <RyandotSilva@gmail.com>
2023-12-11 19:39:32 +0000

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+use reqwest::Client as HttpClient;
+
+use async_trait::async_trait;
+
+use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+use std::sync::Mutex;
+
 fn main() {
     println!("Hello, world!");
 }
